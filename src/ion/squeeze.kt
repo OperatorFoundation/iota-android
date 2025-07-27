e3f88e73a0506391c4df83d59f50408f452ae7c5
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-07-27 01:28:00 -0400

    Reduced code size by using more idomatic Kotlin

--- src/ion/squeeze.kt
+++ src/ion/squeeze.kt
@@ -1,505 +1,165 @@
 import ion.Connection
 import java.nio.ByteBuffer
 
-// Encode an integer in the fewest number of bytes, plus a 1-byte length to tell us how many bytes that is.
-fun squeeze_int(value: Int): ByteArray
+fun squeeze_int(v: Int): ByteArray
 {
-  // The only case where the length can be zero is for the number 0.
-  if (value == 0)
+  if (v == 0) return byteArrayOf(0)
+  val neg = v < 0
+  var pos = if (neg) -v else v
+  val b = mutableListOf<Byte>()
+  while (pos != 0)
   {
-    return byteArrayOf(0)
+    b.add(0, (pos and 0xFF).toByte())
+    pos = pos ushr 8
   }
+  var len = b.size.toByte()
+  if (neg) len = (len.toInt() or 0x80).toByte()
+  return byteArrayOf(len) + b
+}
 
-  // Remember if the integer was positive or negative. If negative, we will make the length negative later.
-  // Regardless, we end up with a positive integer for encoding.
-  // Positive integers are more straightforward to encode.
-  val negative = value < 0
-  var positiveValue = if (negative) -value else value
-
-  // Determine how many bytes we need to encode the integer by removing zero bytes from the top end.
-  // Note that this algorithm does not care about endianness and accommodates any word size.
+fun squeeze_bigint(v: List<Int>): ByteArray
+{
+  val neg = v.first() != 0
+  val remainingValues = v.drop(1)
   val bytes = mutableListOf<Byte>()
 
-  while (positiveValue != 0)
-  {
-    // Get each byte of the integer separately so that we can count how many bytes we need.
-    bytes.add(0, (positiveValue and 0xFF).toByte())
-    positiveValue = positiveValue ushr 8
-  }
-
-  var length = bytes.size.toByte()
-
-  // If the integer is negative, we make the length byte negative as well
-  // The length byte goes at the beginning of the encoding
-  if (negative)
-  {
-    length = (length.toInt() or 0x80).toByte()
-  }
-
-  // Create result array with length byte at the beginning
-  val result = ByteArray(bytes.size + 1)
-  result[0] = length
-  for (i in bytes.indices)
-  {
-    result[i + 1] = bytes[i]
-  }
-
-  return result
-}
-
-// Encode a big integer (represented as List<Int>) in bytes, plus a 1-byte length
-fun squeeze_bigint(value: List<Int>): ByteArray
-{
-  // Remember if the integer was positive or negative. If negative, we will make the length negative later.
-  // Regardless, we end up with a positive integer for encoding.
-  // Positive integers are more straightforward to encode.
-  val negative = value.first() != 0
-  val remainingValue = value.drop(1) // Remove first element (equivalent to erase(begin()))
-
-  val result = mutableListOf<Byte>()
-
-  for(signedInteger in remainingValue)
+  for (signedInteger in remainingValues)
   {
     var unsignedInteger = signedInteger.toUInt()
-
-    val integerBytes = mutableListOf<Byte>()
-
-    // Process all bytes of the integer (4 bytes for Int)
-    repeat(Int.SIZE_BYTES)
+    for (j in 0 until Int.SIZE_BYTES)
     {
-      // Get each byte of the integer separately, encoding all bytes, including zeros
-      integerBytes.add(0, (unsignedInteger and 0xFFu).toByte())
+      val b = (unsignedInteger and 0xFFu).toByte()
+      bytes.add(0, b)
       unsignedInteger = unsignedInteger shr 8
     }
-
-    result.addAll(integerBytes)
   }
 
-  var length = result.size.toByte()
-
-  // If the integer is negative, we make the length byte negative as well
-  // The length byte goes at the beginning of the encoding
-  if(negative)
-  {
-    length = (length.toInt() or 0x80).toByte()
-  }
-
-  // Create final result with length byte at the beginning
-  val finalResult = ByteArray(result.size + 1)
-  finalResult[0] = length
-  for(i in result.indices)
-  {
-    finalResult[i + 1] = result[i]
-  }
-
-  return finalResult
+  var len = bytes.size.toByte()
+  if (neg) len = (len.toInt() or 0x80).toByte()
+  return byteArrayOf(len) + bytes
 }
 
-
-// Convert a Varint to bytes using appropriate squeeze function
-//fun squeezeVarint(value: Varint): ByteArray
-//{
-//  return when (value)
-//  {
-//    is Varint.IonInt ->
-//    {
-//      val integer = value.value
-//      squeeze_int(integer)
-//    }
-//
-//    is Varint.IonInts ->
-//    {
-//      val integers = value.value
-//      squeeze_ints(integers)  // Assuming you have this function
-//    }
-//  }
-//}
-
-// Expand a byte array into a Varint and remaining bytes
-fun expand_int(value: ByteArray): Pair<Varint, ByteArray>
+fun expand_int(v: ByteArray): Pair<Varint, ByteArray>
 {
-  // If input array is empty, we have nothing to parse
-  if(value.isEmpty())
+  if (v.isEmpty()) return Pair(Varint.IonInt(0), byteArrayOf())
+  var len = v[0].toUByte().toInt()
+  if (len == 0) return Pair(Varint.IonInt(0), v.sliceArray(1 until v.size))
+  val neg = (len and 0x80) != 0
+  if (neg) len = len and 0x7F
+  if (v.size < 1 + len) return Pair(Varint.IonInt(0), byteArrayOf())
+  val data = v.sliceArray(1 until 1 + len)
+  val rest = v.sliceArray(1 + len until v.size)
+  var i = expand_int_from_bytes(data)
+  if (neg) i = when (i)
   {
-    return Pair(Varint.IonInt(0), byteArrayOf())
+    is Varint.IonInt -> Varint.IonInt(-i.value)
+    is Varint.IonInts -> Varint.IonInts(listOf(-i.value.first()) + i.value.drop(1))
   }
-
-  // The first byte of the input array is the length at the beginning of the array 
-  // with the integer for us to parse, the rest of the array is extra
-  var integerLength = value.first().toUByte().toInt()
-
-  // The only case where the length can be zero is for the number 0.
-  if(integerLength == 0)
-  {
-    val rest = value.sliceArray(1 until value.size)
-    return Pair(Varint.IonInt(0), rest)
-  }
-
-  // We encode if the integer is positive or negative by using a positive or negative length
-  val negative = (integerLength and 0x80) != 0 // Check the sign bit & 0b10000000
-  if(negative)
-  {
-    integerLength = integerLength and 0x7F // Clear the sign bit & 0b01111111
-  }
-
-  if(value.size < 1 + integerLength)
-  {
-    return Pair(Varint.IonInt(0), byteArrayOf())
-  }
-
-  val integerData = value.sliceArray(1 until 1 + integerLength)
-  val rest = value.sliceArray(1 + integerLength until value.size)
-
-  var i = expand_int_from_bytes(integerData)
-
-  if(negative)
-  {
-    i = when (i)
-    {
-      is Varint.IonInt -> 
-      {
-        val integer = i.value
-        Varint.IonInt(-integer)
-      }
-      is Varint.IonInts -> 
-      {
-        val integers = i.value.toMutableList()
-        integers[0] = -integers[0]
-        Varint.IonInts(integers)
-      }
-    }
-  }
-
   return Pair(i, rest)
 }
 
-// Expand a varint from a Connection
-fun expand_conn(conn: Connection): Varint
+fun expand_conn(c: Connection): Varint
 {
-  var length = conn.readOne().toUByte().toInt()
-
-  if(length == 0)
+  var len = c.readOne().toUByte().toInt()
+  if (len == 0) return Varint.IonInt(0)
+  val neg = (len and 0x80) != 0
+  if (neg) len = len and 0x7F
+  var i = expand_int_from_bytes(c.read(len))
+  return when (i)
   {
-    return Varint.IonInt(0)
-  }
-
-  val negative = (length and 0x80) != 0
-  if(length and 0x80 != 0)
-  {
-    length = length and 0x7F
-  }
-
-  val integerBytes = conn.read(length)
-
-  var i = expand_int_from_bytes(integerBytes)
-
-  when(i)
-  {
-    is Varint.IonInt ->
-    {
-      if(negative)
-      {
-        val integer = i.value
-        i = Varint.IonInt(-integer)
-      }
-    }
-    is Varint.IonInts ->
-    {
-      val integers = i.value.toMutableList()
-
-      if(negative)
-      {
-        integers.add(0, 1)
-      }
-      else
-      {
-        integers.add(0, 0)
-      }
-
-      i = Varint.IonInts(integers)
-    }
-  }
-
-  return i
-}
-
-// Expand bytes into a Varint (inverse of squeeze operations)
-fun expand_int_from_bytes(bytes: ByteArray): Varint
-{
-  // We have to use an unsigned int here because in the case that we end up with a bigint, 
-  // all the limbs should be unsigned. If we use a (signed) int, we will lose the top bit.
-  val integers = mutableListOf<Int>()
-
-  for(count in 1..bytes.size)
-  {
-    // We ran out of bytes in the queued integers
-    if(count % Int.SIZE_BYTES == 1)
-    {
-      integers.add(0, 0)
-    }
-
-    // Shift all the integers one byte left
-    for(index in 0 until integers.size)
-    {
-      if(index == integers.size - 1)
-      {
-        integers[index] = ((integers[index].toUInt() shl 8) or bytes[count - 1].toUByte().toUInt()).toInt()
-      }
-      else
-      {
-        val current = integers[index].toUInt()
-        val next = integers[index + 1].toUInt()
-
-        val shift = (Int.SIZE_BYTES - 1) * 8
-        val nextHighByte = (next shr shift) and 0xFFu
-
-        integers[index] = ((current shl 8) or nextHighByte).toInt()
-      }
-    }
-  }
-
-  // In this special case, we might have a valid max size (signed) int, or we might have 
-  // a max size unsigned int that is too big to fit into a signed int because of the sign bit.
-  if(integers.size == 1)
-  {
-    val unsignedInteger = integers[0].toUInt()
-
-    if(unsignedInteger <= Int.MAX_VALUE.toUInt())
-    {
-      return Varint.IonInt(unsignedInteger.toInt())
-    }
-  }
-
-  return Varint.IonInts(integers)
-}
-
-// Serialize floating point numbers as big endian byte arrays
-fun squeeze_floating(value: Floating): ByteArray
-{
-  return when(value)
-  {
-    is Floating.IonFloat ->
-    {
-      val f = value.value
-
-      if(f == 0.0f)
-      {
-        byteArrayOf(0)
-      }
-      else
-      {
-        val result = mutableListOf<Byte>()
-        result.add(Float.SIZE_BYTES.toByte()) // 4 bytes for float
-
-        val buffer = ByteBuffer.allocate(Float.SIZE_BYTES)
-        buffer.putFloat(f) // Uses big endian by default
-        val floatBytes = buffer.array()
-
-        result.addAll(floatBytes.toList())
-        result.toByteArray()
-      }
-    }
-    is Floating.IonDouble ->
-    {
-      val d = value.value
-
-      if(d == 0.0)
-      {
-        byteArrayOf(0)
-      }
-      else
-      {
-        val result = mutableListOf<Byte>()
-        result.add(Double.SIZE_BYTES.toByte()) // 8 bytes for double
-
-        val buffer = ByteBuffer.allocate(Double.SIZE_BYTES)
-        buffer.putDouble(d) // Uses big endian by default
-        val doubleBytes = buffer.array()
-
-        result.addAll(doubleBytes.toList())
-        result.toByteArray()
-      }
-    }
+    is Varint.IonInt -> if (neg) Varint.IonInt(-i.value) else i
+    is Varint.IonInts -> Varint.IonInts(listOf(if (neg) 1 else 0) + i.value)
   }
 }
 
-// Deserialize floating point numbers from big endian byte arrays
-fun expand_floating(value: ByteArray): Floating?
+fun expand_int_from_bytes(b: ByteArray): Varint
 {
-  if(value.isEmpty())
+  val ints = mutableListOf<Int>()
+  for (c in 1..b.size)
   {
-    return null
-  }
-
-  val length = value[0].toUByte().toInt()
-
-  if(length == 0)
-  {
-    return Floating.IonFloat(0.0f)
-  }
-
-  if(value.size < 1 + length)
-  {
-    return null
-  }
-
-  val floatBytes = value.sliceArray(1 until 1 + length)
-
-  return when(length)
-  {
-    4 ->
+    if (c % Int.SIZE_BYTES == 1) ints.add(0, 0)
+    for (i in ints.indices)
     {
-      val buffer = ByteBuffer.wrap(floatBytes) // Uses big endian by default
-      val f = buffer.float
-      Floating.IonFloat(f)
+      ints[i] = if (i == ints.size - 1)
+        ((ints[i].toUInt() shl 8) or b[c - 1].toUByte().toUInt()).toInt()
+      else
+      {
+        val cur = ints[i].toUInt()
+        val next = ints[i + 1].toUInt()
+        ((cur shl 8) or ((next shr 24) and 0xFFu)).toInt()
+      }
     }
-    8 ->
-    {
-      val buffer = ByteBuffer.wrap(floatBytes) // Uses big endian by default
-      val d = buffer.double
-      Floating.IonDouble(d)
-    }
+  }
+  return if (ints.size == 1 && ints[0].toUInt() <= Int.MAX_VALUE.toUInt())
+    Varint.IonInt(ints[0]) else Varint.IonInts(ints)
+}
+
+fun squeeze_floating(v: Floating): ByteArray = when (v)
+{
+  is Floating.IonFloat -> if (v.value == 0.0f) byteArrayOf(0) else
+    byteArrayOf(4) + ByteBuffer.allocate(4).putFloat(v.value).array()
+  is Floating.IonDouble -> if (v.value == 0.0) byteArrayOf(0) else
+    byteArrayOf(8) + ByteBuffer.allocate(8).putDouble(v.value).array()
+}
+
+fun expand_floating(v: ByteArray): Floating?
+{
+  if (v.isEmpty()) return null
+  val len = v[0].toUByte().toInt()
+  if (len == 0) return Floating.IonFloat(0.0f)
+  if (v.size < 1 + len) return null
+  return when (len)
+  {
+    4 -> Floating.IonFloat(ByteBuffer.wrap(v, 1, 4).float)
+    8 -> Floating.IonDouble(ByteBuffer.wrap(v, 1, 8).double)
     else -> null
   }
 }
 
-// Deserialize floating point numbers from a Connection
-fun expand_conn_floating(conn: Connection): Floating?
+fun expand_conn_floating(c: Connection): Floating?
 {
-  val length = conn.readOne().toUByte().toInt()
-
-  if(length == 0)
+  val len = c.readOne().toUByte().toInt()
+  if (len == 0) return Floating.IonFloat(0.0f)
+  val b = c.read(len)
+  return when (len)
   {
-    return Floating.IonFloat(0.0f)
-  }
-
-  val floatBytes = conn.read(length)
-
-  return when(length)
-  {
-    Float.SIZE_BYTES ->
-    {
-      val buffer = ByteBuffer.wrap(floatBytes) // Uses big endian by default
-      val f = buffer.float
-      Floating.IonFloat(f)
-    }
-    Double.SIZE_BYTES ->
-    {
-      val buffer = ByteBuffer.wrap(floatBytes) // Uses big endian by default
-      val d = buffer.double
-      Floating.IonDouble(d)
-    }
+    4 -> Floating.IonFloat(ByteBuffer.wrap(b).float)
+    8 -> Floating.IonDouble(ByteBuffer.wrap(b).double)
     else -> null
   }
 }
 
-// Encode an array of integers in the fewest number of bytes, plus an encoded integer length to tell us how many bytes that is.
-fun squeeze_ints(value: List<Int>): ByteArray
+fun squeeze_ints(v: List<Int>): ByteArray = if (v.isEmpty()) byteArrayOf(0) else
+  squeeze_int(v.size).toList().plus(v.flatMap
+  { it: Int -> squeeze_int(it).toList() }).toByteArray()
+
+fun expand_ints(v: ByteArray): Pair<List<Int>, ByteArray>
 {
-  // The only case where the length can be zero is for the number 0.
-  if(value.isEmpty())
-  {
-    return byteArrayOf(0)
-  }
-
-  // Allocate the result
-  val result = mutableListOf<Byte>()
-
-  val size = value.size
-  val sizeBytes = squeeze_int(size)
-
-  result.addAll(sizeBytes.toList())
-
-  for(integer in value)
-  {
-    val valueBytes = squeeze_int(integer)
-    result.addAll(valueBytes.toList())
-  }
-
-  return result.toByteArray()
-}
-
-// Expand a byte array into a List<Int> and remaining bytes
-fun expand_ints(value: ByteArray): Pair<List<Int>, ByteArray>
-{
-  // If input array is empty, we have nothing to parse
-  if(value.isEmpty())
-  {
-    return Pair(emptyList(), value)
-  }
-
-  val integers = mutableListOf<Int>()
-
-  // The first byte of the input array is the length at the beginning of the array with the integer for us to parse, the rest of the array is extra
-  val sizeResult = expand_int(value)
-  val varsize = sizeResult.first
-  var rest = sizeResult.second
-
-  when(varsize)
+  if (v.isEmpty()) return Pair(emptyList(), v)
+  val (sz, rest1) = expand_int(v)
+  return when (sz)
   {
     is Varint.IonInt ->
     {
-      val size = varsize.value
-
-      // The only case where the length can be zero is for empty array [].
-      if(size == 0)
+      if (sz.value == 0) return Pair(emptyList(), rest1)
+      var r = rest1
+      val ints = mutableListOf<Int>()
+      for (i in 0 until sz.value)
       {
-        return Pair(emptyList(), rest)
-      }
-
-      for(count in 0 until size)
-      {
-        val intResult = expand_int(rest)
-        val varinteger = intResult.first
-        rest = intResult.second
-
-        when(varinteger)
+        val (int, rest2) = expand_int(r)
+        r = rest2
+        when (int)
         {
-          is Varint.IonInt ->
-          {
-            val integer = varinteger.value
-            integers.add(integer)
-          }
-          is Varint.IonInts ->
-          {
-            // varint elements in integer arrays are not yet fully implemented
-            return Pair(emptyList(), byteArrayOf())
-          }
+          is Varint.IonInt -> ints.add(int.value)
+          else -> return Pair(emptyList(), byteArrayOf())
         }
       }
-
-      return Pair(integers, rest)
+      Pair(ints.toList(), r)
     }
-    is Varint.IonInts ->
-    {
-      // varint sizes are not yet fully implemented
-      return Pair(emptyList(), byteArrayOf())
-    }
+    else -> Pair(emptyList(), byteArrayOf())
   }
 }
 
-// Encode an array of floats in the fewest number of bytes, plus an encoded integer length to tell us how many bytes that is.
-fun squeeze_floats(value: List<Float>): ByteArray
-{
-  // The only case where the length can be zero is for the number 0.
-  if(value.isEmpty())
-  {
-    return byteArrayOf(0)
-  }
-
-  // Allocate the result
-  val result = mutableListOf<Byte>()
-
-  val size = value.size
-  val sizeBytes = squeeze_int(size)
-
-  result.addAll(sizeBytes.toList())
-
-  for(f in value)
-  {
-    val valueBytes = squeeze_floating(Floating.IonFloat(f))
-    result.addAll(valueBytes.toList())
-  }
-
-  return result.toByteArray()
-}
+fun squeeze_floats(v: List<Float>): ByteArray = if (v.isEmpty()) byteArrayOf(0) else
+  squeeze_int(v.size).toList().plus(v.flatMap
+  { it: Float -> squeeze_floating(Floating.IonFloat(it)).toList() }).toByteArray()
